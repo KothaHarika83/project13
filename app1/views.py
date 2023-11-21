7980d4c1b53c84e432e872460202f2d09532d15d
kothaharika83 <harikakotha83@gmail.com>
2023-11-21 14:57:53 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def harika(request):
+    return render(request,'harika.html')
+
+def joshna(request):
+    return render(request,'joshna.html')
+
+def mother(request):
+    return render(request,'mother.html')
